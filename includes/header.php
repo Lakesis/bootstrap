b752ccf19d277650f466de3455f575b10247f267
Pablo Gil <lakesis@gmail.com>
2012-07-27 11:37:27 +1000

    Fixes

--- includes/header.php
+++ includes/header.php
@@ -1,6 +1,7 @@
 <?php
 	function scripts(){
 		echo 	'<script src="js/jquery-1.7.2.min.js"></script>
+				<script src="js/project.js"></script>
 				';
 	}
 ?>
@@ -9,14 +10,16 @@
 	<head>
 		<meta charset="utf-8">
 		<title></title>
+		
 		<link rel="stylesheet" type="text/less" media="screen,projection,print" href="css/style.less">
 		<link rel="stylesheet" media="print" href="css/print.css">
 		<link rel="stylesheet" media="handheld,screen and (max-device-width: 480px)" href="css/handheld.css">
+		<script src="dev/less-1.3.0.min.js"></script>
 		<!--[if lt IE 9]><script src="js/html5-shiv.js"></script><![endif]-->
 		
 		<link rel="shortcut icon" href="images/favicon.png">
 	</head>
-	<body>
+	<body data-section="" data-step="">
 		<div id="pageWrapper">
 			
 			<header>
--- js/project.js
+++ js/project.js
@@ -1,4 +1,20 @@
 var PROJECT = (function(PROJECT, $, window){
+	var section,
+	step;
+	
+	var init = function(){
+		var mapping = {
+			
+		};
+	
+		$(document).ready(function(){
+			section = $('body').data('section');
+			step = $('body').data('step');
+			
+			mapping[section]();
+			
+		});
+	}();
 
 	return PROJECT;
 	
